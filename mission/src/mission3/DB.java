7ee4d9a0c62a6bf14c0ea25a228c6ac64c1aa892
bibi6666667 <bibi6666667@kakao.com>
2020-11-10 17:55:00 +0900

    [가계부] 클래스명 수정 및 알고리즘 변경.
    -메인 클래스 AccountBook 을MoneyBook 으로 이름을 변경했다.
    - loginHome과 moneybookHome을 만들고, 다른 메서드 수행 후 되돌아오도록 연결했다.
    - [CRUD]
    아직 create밖에 못 했는데 빨리 나머지도 하고 싶다.ㅠㅠ
    그러기 위해서는 ArrayList를 더 공부해야 할 것 같다.
    내일 미션도 가계부 발전과제이니 이어서 하자.

--- mission/src/mission3/DB.java
+++ mission/src/mission3/DB.java
@@ -1,6 +1,7 @@
 package mission3;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -13,7 +14,7 @@ public class DB {
     public ArrayList<String[]> AccountDB = new ArrayList<>(); // 문자열 배열을 넣는 리스트로 선언.
     //ArrayList에는 참조변수만 선언 가능.
 
-
+    MoneyBook moneyBook = new MoneyBook();
     Scanner sc = new Scanner(System.in);
 
 
@@ -23,10 +24,11 @@ public class DB {
         String[] newIdPwArr = IdPwArr;
         String id = newIdPwArr[0];
         String pw = newIdPwArr[1];
-        System.out.println(id + "," + pw + "로 가입합니다.");
         IdPwDB.put(id, pw);
-        System.out.println("현재 회원 : " + IdPwDB);
-        AccountBook.login();
+        System.out.println(id + "," + pw + "로 가입이 완료되었습니다");
+        System.out.println("현재 회원 목록 : " + IdPwDB);
+        System.out.println("로그인으로 이동합니다.");
+        moneyBook.login();
     }
     
     public boolean checkMembersDB(String[] IdPwArr) {
@@ -49,35 +51,44 @@ public class DB {
         return false;
     }
 
+    // 로그인된 id값을 반환하는 함수
+
     //////////////////////2. 가계부 DB 관련 메서드-CRUD //////////////////////////
     // (1) Create 생성
     public void createAccountDB(){
         // 날짜, 적요, 수입, 지출 입력받기
-        System.out.println("C-가계부 쓰기");
+        System.out.println("--가계부를 작성합니다--");
         System.out.println("가계부에 새 내역을 작성합니다. 날짜, 설명, 수입, 지출 순으로 입력해 주세요");
-        System.out.println("날짜를 입력해 주세요(연,월,일의 8자리 숫자로 입력) > ");
-        String year = sc.next();
-        System.out.println("수입/지출에 대한 간단한 설명을 입력해 주세요 > ");
-        String descript = sc.next();
-        System.out.println("수입이 얼마였는지 숫자로 입력해 주세요 > ");
-        String income = sc.next();
-        System.out.println("지출은 얼마였는지 숫자로 입력해 주세요 > ");
-        String expend = sc.next();
-        System.out.println(year);
-        System.out.println(descript);
-        System.out.println(income);
-        System.out.println(expend);
+        System.out.println("날짜를 입력해 주세요(연,월,일의 8자리 숫자로 입력)");
+        System.out.print(">");
+         String date = sc.next();
+        System.out.println("수입/지출에 대한 간단한 설명을 입력해 주세요");
+        System.out.print(">");
+         String descript = sc.next();
+        System.out.println("수입이 얼마였는지 숫자로 입력해 주세요");
+        System.out.print(">");
+         String income = sc.next();
+        System.out.println("지출은 얼마였는지 숫자로 입력해 주세요");
+        System.out.print(">");
+         String expend = sc.next();
         // 순번 추가
         String numbering = AccountNum;
-        // AccountNum++;
+        int intAccountNum = Integer.parseInt(AccountNum); // 숫자로 바꿔서 1 더하기
+        intAccountNum++;
+        AccountNum = Integer.toString(intAccountNum); // 다시 문자로 형변환
+        System.out.println(numbering);
+        System.out.println(AccountNum);
+
         // 잔액 계산, 추가
         // 회원명 추가
+        String user = MoneyBook.ID;
+        System.out.println(user);
         // 저장
-        String[] anAccount = new String[5]; // 6으로 고치기!
-        // anAccount = {numbering, year, descript, income, expend};
+        String[] anAccount = new String[]{numbering, date, descript, income, expend, user};
         AccountDB.add(anAccount);
-        // 차라리 전부 String으로 받는 게 나을 듯
-        // 또는 cooper가 알려준 대로 생성자 만들어서 보내기?
+        System.out.println(Arrays.toString(anAccount) + " 로 저장되었습니다.");
+        System.out.println("HOME으로 돌아갑니다.");
+        moneyBook.moneybookHome();
 
         //연도 리스트
         //설명 리스트
@@ -86,35 +97,31 @@ public class DB {
 
     }
 
-    // (2) Read 조회
+    // (2) Read 조회 (회원명은 출력하지 않음)
     public void readAccountDB(){
-        System.out.println("R-가계부 읽기");
-        System.out.println("번호 | 날짜 | 내용 | 수입 | 지출 | 회원명");
-        System.out.println("새 항목을 작성하려면 1, 기존 항목을 수정하려면 2, 기존 항목을 삭제하려면 3을 입력해 주세요 >");
-        int inputNum = sc.nextInt();
-        switch(inputNum) {
-            case 1 :
-                createAccountDB();
-                break;
-            case 2 :
-                updateAccountDB();
-                break;
-            case 3 : 
-                deleteAccountDB();
-                break;
-        }
+        System.out.println("--가계부를 읽어옵니다--");
+        System.out.println("번호 | 날짜 | 내용 | 수입 | 지출 ");
+        // AccountDB에서 읽어오기
+        moneyBook.moneybookHome();
+
 
     }
-    // (2-1) 잔액 계산
+    // (2-1) 잔액 계산, 출력
 
     // (3) Update 수정
     public void updateAccountDB(){
-        System.out.println("U-가계부 수정하기");
+        System.out.println("--가계부를 수정합니다--");
+        // AccountDB에서 수정하기
+        System.out.println("HOME으로 돌아갑니다.");
+        moneyBook.moneybookHome();
     }
 
     // (4) Delete 삭제
     public void deleteAccountDB(){
         System.out.println("D-가계부 삭제하기");
+        // AccountDB에서 수정하기
+        System.out.println("HOME으로 돌아갑니다.");
+        moneyBook.moneybookHome();
     }
 
 
---
+++ mission/src/mission3/MoneyBook.java
@@ -0,0 +1,136 @@
+package mission3;
+
+import java.util.Scanner;
+
+public class MoneyBook {
+    static DB db = new DB();// 다른 class파일 불러오기
+
+    public static void main(String[] args) {
+        MoneyBook moneyBook = new MoneyBook();
+        moneyBook.loginHome();
+    }
+
+    public static String ID;
+    public static Scanner sc = new Scanner(System.in);
+
+
+    // 매 선택마다 home, 종료 버튼 추가하기. (로그인 홈 / 가계부 목록)
+    public void loginHome() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("== 가계부입니다 ==");
+        System.out.println("아무 숫자나 입력해 로그인 하세요.");
+        System.out.println("처음이라면, 0 을 입력해 회원가입으로 이동합니다.");
+        System.out.println("4를 누르면 종료됩니다.");
+        System.out.print(">");
+        int inputNum = sc.nextInt();
+        switch (inputNum) {
+            case 0 :   // 회원가입
+                register();
+                break;
+            case 4 : // 종료
+                System.out.println("또 오세요!");
+                break;
+            default :  // 로그인
+                login(); // Login.java 연결이 안됨..
+                break;
+        }
+    }
+
+    public String[] register(){
+
+        // 입력받기
+        System.out.println("--회원가입--");
+        System.out.println("아이디를 입력하세요");
+        System.out.print(">");
+        String id = sc.next();
+        System.out.println("비밀번호를 입력하세요");
+        System.out.print(">");
+        String pw = sc.next();
+        // DB에 추가하기
+        String[] newIdPwArr = {id, pw};
+        db.makeMembersDB(newIdPwArr);
+        return newIdPwArr;
+    }
+
+    public String[] login(){
+        Scanner sc = new Scanner(System.in);
+        // 입력받기
+        System.out.println("--로그인--");
+        System.out.println("아이디를 입력하세요");
+        System.out.print(">");
+        String id = sc.next();
+        System.out.println("비밀번호를 입력하세요");
+        System.out.print(">");
+        String pw = sc.next();
+        ID = id;
+        // DB와 비교하기
+        String[] IdPwArr = {id, pw};
+        boolean checkResult = db.checkMembersDB(IdPwArr);
+        if (checkResult == true) {
+            // true이면 가계부 데이터에 접근
+            moneybookHome();
+        }
+        if (checkResult == false) {
+            // false이면 다시 로그인 또는 회원가입
+            System.out.println("아이디 또는 비밀번호가 일치하지 않습니다. ");
+            System.out.println("아무 숫자나 눌러 재시도하거나, 0을 눌러 회원가입을 하세요");
+            System.out.print(">");
+            int input = sc.nextInt();
+            if (input == 0) {
+                register();
+            }
+            if (input != 0) {
+                login();
+            }
+        }
+        return IdPwArr;
+    }
+
+/////////////////////////////////////////////
+
+    public void moneybookHome(){
+        System.out.println("[HOME] 가계부에 오신 것을 환영합니다. 실행을 원하는 숫자를 눌러주세요.");
+        System.out.println("0 : 내용 작성, 1 : 내용 조회, 2 : 내용 수정, 3 : 내용 삭제, 4 : 종료");
+        System.out.println("다른 숫자를 누르면 로그아웃 후 로그인 화면으로 돌아갑니다.");
+        System.out.print(">");
+        int inputNum = sc.nextInt();
+        switch(inputNum) {
+            case 0 :
+                db.createAccountDB();
+                break;
+            case 1 :
+                db.readAccountDB();
+                break;
+            case 2 :
+                db.updateAccountDB();
+                break;
+            case 3 :
+                db.deleteAccountDB();
+                break;
+            case 4 :
+                System.out.println("또 오세요!");
+                return;
+            default:
+                loginHome();
+        }
+    }
+
+    public void create() {
+
+    }
+
+    public void read() {
+
+    }
+
+    public void update(){
+
+    }
+
+    public void delete(){
+
+    }
+}
+
+
+
