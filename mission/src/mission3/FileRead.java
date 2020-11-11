b61f2a57ec10e85647871ac4365b453a21525666
bibi6666667 <bibi6666667@kakao.com>
2020-11-11 15:31:49 +0900

    [test] 파일 입출력 테스트용 예제.
    https://wikidocs.net/227 의 예제를 보고 따라했다.
    단, 예외처리는 위 예제와 달리 try catch문을 사용했다.
    throw IOException을 사용하니 예외처리를 넘겨받은 다른 메소드에서도 계속 예외처리를 해줘야 해서.. ㅜ

---
+++ mission/src/mission3/FileRead.java
@@ -0,0 +1,19 @@
+package mission3;
+// 'txt 파일 출력(데이터 읽어오기)' 테스트용 파일입니다.
+
+import java.io.BufferedReader;
+import java.io.FileReader; // BufferedReader와 FileReader를 조합해 사용하면 txt파일을 한 줄씩 읽을 수 있다.
+import java.io.IOException;
+
+
+public class FileRead {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("c:/out.txt"));
+        while (true) { // .txt 파일을 라인 단위로 읽어와서, 라인 단위로 출력.
+            String line = bufferedReader.readLine(); // .readline()메소드 : 더 이상 읽을 라인이 없으면 null을 리턴함.
+            if (line == null) break; // 더 이상 읽을 라인이 없으면 while문을 빠져나감.
+            System.out.println(line);
+        }
+        bufferedReader.close();
+    }
+}
---
+++ mission/src/mission3/FileWrite.java
@@ -0,0 +1,27 @@
+package mission3;
+// 'txt 파일 입력(데이터 넣기)' 테스트용 파일입니다.
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWrite {
+    public static void main(String[] args) throws IOException{
+        // 파일을 생성하기 위해 FileOutputStream 클래스 사용해 output 객체를 생성.
+        FileWriter fw = new FileWriter("c:/out.txt");
+        // 파일에 넣고 싶은 내용 작성 : .write(내용);
+        for(int i=1; i<11; i++) {
+            String data = i+" 번째 고양이!🐱"+"\r\n";// \r\n은 줄바꿈 문자이다.
+            fw.write(data);
+        }
+        fw.close();// 꼭 사용 후 닫아주기.
+
+        // 기존 파일에 새로운 내용 추가하기 : 파일명 뒤에 추가모드(appenr)를 true로 한다.
+        FileWriter fw2 = new FileWriter("c:/out.txt", true);
+        for(int i=11; i<21; i++) {
+            String data = "(추가)"+i+" 번째 고양이!🐱\r\n";
+            fw2.write(data);
+        }
+        fw2.close();
+    }
+
+}
