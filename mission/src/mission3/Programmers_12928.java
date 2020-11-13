03aed3e073c6f638ba68057db758c08d41d8abd8
bibi6666667 <bibi6666667@kakao.com>
2020-11-13 12:13:48 +0900

    [알고리즘] 프로그래머스 '약수의 합'
    https://programmers.co.kr/learn/courses/30/lessons/12928 문제 풀이입니다.

---
+++ mission/src/mission3/Programmers_12928.java
@@ -0,0 +1,23 @@
+package mission3;
+
+public class Programmers_12928 {
+    public static void main (String[] args) {
+        Programmers_12928 p = new Programmers_12928();
+        p.solution(12); // n = 12일 때 answer = 28
+    }
+
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n ; i++) {
+            if (n % i == 0) {
+                answer += i;
+                System.out.println(answer);
+            }
+        }
+        return answer;
+    }
+
+}
+
+
+
