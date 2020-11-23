848368bf196dc95d1dc5802dfccc956d26810118
bibi6666667 <bibi6666667@kakao.com>
2020-11-23 17:58:43 +0900

    [AWT 학습] TextField 예제 실습
    짧은 값을 입력받는 TextField
    -setEchoChar로 비밀번호 등을 세팅할 수 있다.

---
+++ mission/src/mission6/TextFieldTest.java
@@ -0,0 +1,27 @@
+package mission6;
+
+import java.awt.*;
+
+public class TextFieldTest {
+    public static void main(String[] args) {
+        Frame f = new Frame("Login");
+        f.setSize(400,65);
+        // 레이아웃매니저를 FlowLayout로 한다.
+        f.setLayout(new FlowLayout());
+
+        Label lid = new Label("ID : ", Label.RIGHT); // 오른쪽 정렬
+        Label lpwd = new Label("Password : ", Label.RIGHT);
+
+        TextField id = new TextField(10); // 10글자를 입력할 수 이는 크기로 생성
+        TextField pwd = new TextField(10); // 10글자를 입력할 수 이는 크기로 생성
+        pwd.setEchoChar('*'); // 입력한 값 대신 *가 보이게 한다.
+        // 한 글자이므로 홑따옴표를 사용해야 한다.
+
+        f.add(lid);
+        f.add(id);
+        f.add(lpwd);
+        f.add(pwd);
+        f.setVisible(true);
+
+    }
+}
