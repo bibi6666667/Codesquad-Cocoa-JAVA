1d9caea6826c794a7aa4c9d5b443c079226c28da
bibi6666667 <bibi6666667@kakao.com>
2020-11-23 18:01:25 +0900

    [AWT 학습] Scrollbar 예제 실습
    스크롤바를 만드는 Scrollbar 실습
    - 가로 스크롤바, 세로 스크롤바
    - 초기값, 스크롤버튼 크기, 스크롤바 최솟값/최댓값

---
+++ mission/src/mission6/ScrollbarTest.java
@@ -0,0 +1,25 @@
+package mission6;
+
+import java.awt.*;
+
+public class ScrollbarTest {
+    public static void main(String[] args) {
+        Frame f = new Frame("Scrollbar");
+        f.setSize(300,200);
+        f.setLayout(null);
+        
+        // 가로 스크롤바
+        Scrollbar hor = new Scrollbar(Scrollbar.HORIZONTAL, 0, 50, 0, 100);
+        hor.setSize(100, 15);
+        hor.setLocation(60,30);
+        
+        // 세로 스크롤바
+        Scrollbar ver = new Scrollbar(Scrollbar.VERTICAL, 50, 20, 0, 100);
+        ver.setSize(15, 100);
+        ver.setLocation(30,30);
+
+        f.add(hor);
+        f.add(ver);
+        f.setVisible(true);
+    }
+}
