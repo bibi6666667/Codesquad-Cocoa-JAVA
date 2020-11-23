fc04673bae85e85db87b7e8fb2e7255b1a5b48fa
bibi6666667 <bibi6666667@kakao.com>
2020-11-23 23:35:12 +0900

    [AWT 학습 정리] ~FileDialog
    - ~FileDialog 까지 공부 및 정리.
    - 소제목을 #####로 했더니 너무 작은 듯 해서 ####로 일괄 변경.
    - * 들여쓰기 잘 안 된 부분들 수정.

---
+++ mission/src/mission6/DialogTest.java
@@ -0,0 +1,25 @@
+package mission6;
+
+import java.awt.*;
+
+public class DialogTest {
+    public static void main(String[] args) {
+        Frame f = new Frame("Parent");
+        f.setSize(300, 200);
+
+        // parent Frame을 f, modal을 true(필수응답 Dialog)
+        Dialog info = new Dialog(f, "Information", true);
+        info.setSize(140, 90);
+        info.setLocation(50,50); // parent Frame이 아닌, 화면이 위치기준이 됨.
+        info.setLayout(new FlowLayout());
+        // Dialog의 기본 레아이웃 매니저는 BorderLayout이기 떄문에 FlowLayout로 변경.
+
+        Label msg = new Label("This is modal Dialog", Label.CENTER);
+        Button ok = new Button("ok");
+        info.add(msg);
+        info.add(ok);
+
+        f.setVisible(true); // Frame을 보이게 한 다음,
+        info.setVisible(true); // Dialog를 보이게 한다.
+    }
+}
---
+++ mission/src/mission6/ScrollPaneTest.java
@@ -0,0 +1,22 @@
+package mission6;
+
+import java.awt.*;
+
+public class ScrollPaneTest {
+    public static void main(String[] args) {
+        Frame f = new Frame("ScrollPaneTest");
+        f.setSize(300, 200);
+
+        ScrollPane sp = new ScrollPane();
+        Panel p = new Panel();
+        p.setBackground(Color.green);
+        p.add(new Button("첫번째")); // Button들을 Panel에 포함시킨다.
+        p.add(new Button("두번째"));
+        p.add(new Button("세번째"));
+        p.add(new Button("네번쨰"));
+
+        sp.add(p); // Panel을 ScrollPane에 포함시킨다
+        f.add(sp); // ScrollPane을 Frame에 포함시킨다
+        f.setVisible(true);
+    }
+}
