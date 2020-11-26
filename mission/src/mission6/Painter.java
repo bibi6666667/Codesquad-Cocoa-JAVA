b6c97cb5815ad6a21d7174a87aa6804562365f91
bibi6666667 <bibi6666667@kakao.com>
2020-11-26 17:39:50 +0900

    [미션8] 그림판 - 화면 깜빡거림 해결 , 기능구현 시도..
    - 그룹피어세션을 하며 가상화면이 있어도 화면이 깜빡거리는 현상을 해결했다! update(Graphics g)를 사용하면 된다.
    - 원, 사각형 그리기 기능을 만드려다가 어떻게 이벤트처리를 해야 하는지 감이 안 와서 포기했다😭..

--- mission/src/mission6/Painter.java
+++ mission/src/mission6/Painter.java
@@ -2,19 +2,21 @@ package mission6;
 
 import java.awt.*;
 import java.awt.event.*;
-import java.beans.EventHandler;
 
 public class Painter extends Frame implements MouseMotionListener {
     // 메뉴컴포넌트
     MenuBar menuBar;
     Menu mPainter, mTools, mColor;
     MenuItem miNew, miExit;
-    MenuItem miPencil, miCircle, miRectangle; //miRoundRectangle, miArc;  다각형까지?
+    MenuItem miPen, miCircle, miRectangle; //miRoundRectangle, miArc;  다각형까지?
     //MenuItem miBlack, miWhite, miRed, miBlue, miGreen, miYellow, miOrange, miPink;
 
     // x y 선언
     int x = 0;
     int y = 0;
+
+    Object source;
+
     // 가상화면 - Image, Graphics
     Image img = null;
     Graphics gImg = null;
@@ -34,13 +36,13 @@ public class Painter extends Frame implements MouseMotionListener {
         mColor = new Menu("Color");
         miNew = new MenuItem("New");
         miExit = new MenuItem("Exit");
-        miPencil = new MenuItem("Pencil");
+        miPen = new MenuItem("Pencil");
         miCircle = new MenuItem("Circle");
         miRectangle = new MenuItem("Rectangle");
         mPainter.add(miNew);
         mPainter.addSeparator();
         mPainter.add(miExit);
-        mTools.add(miPencil);
+        mTools.add(miPen);
         mTools.add(miCircle);
         mTools.add(miRectangle);
 
@@ -48,15 +50,16 @@ public class Painter extends Frame implements MouseMotionListener {
         menuBar.add(mTools);
         setMenuBar(menuBar);
 
+        //메뉴 이벤트처리
         EventHandler eventHandler = new EventHandler();
         miNew.addActionListener(eventHandler);
         miExit.addActionListener(eventHandler);
-        miPencil.addActionListener(eventHandler);
+        miPen.addActionListener(eventHandler);
         miCircle.addActionListener(eventHandler);
         miRectangle.addActionListener(eventHandler);
-
-        //종료버튼
+        //마우스포인터 이벤트처리
         addMouseMotionListener(this);
+        //종료 이벤트처리
         addWindowListener(new WindowAdapter() {// 종료버튼 눌렀을 때.
             public void windowClosing(WindowEvent e) {
                 System.exit(0); // 시스템 종료
@@ -77,6 +80,10 @@ public class Painter extends Frame implements MouseMotionListener {
         if (img == null) return;
         g.drawImage(img, 0, 0, this); // img(가상화면)에 그려진 그림을 Frame에 복사
     }
+    // ↓ update()를 추가하면 이미지 깜빡거리지 않아요.
+    public void update(Graphics g){
+        paint(g);
+    }
 
     //이벤트처리
     @Override
@@ -88,24 +95,45 @@ public class Painter extends Frame implements MouseMotionListener {
     @Override
     public void mouseDragged(MouseEvent e) {
         if (e.getModifiersEx() != MouseEvent.BUTTON1_DOWN_MASK) return;
-
         gImg.drawLine(x, y, e.getX(), e.getY());
         x = e.getX();
         y = e.getY();
         repaint();
     }
 
-    class EventHandler implements ActionListener {
+    public void mouseClicked(MouseEvent e) {
+        source = e.getSource();
+        System.out.println(source);
+        if (e.getModifiersEx() != MouseEvent.BUTTON1_DOWN_MASK) return;
+        gImg.drawLine(x, y, e.getX(), e.getY());
+        x = e.getX();
+        y = e.getY();
+        repaint();
+    }
+
+    // 메뉴 핸들러
+   class EventHandler implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
             String command = e.getActionCommand();
-            if (command.equals("New")){
-                gImg.clearRect(0,0,640,480);
-                repaint();
-            } else if (command.equals("Exit")) {
-                System.exit(0);
+            switch (command) {
+                case "New" :
+                    gImg.clearRect(0,0,640,480);
+                    repaint();
+                    break;
+                case "Exit" :
+                    System.exit(0);
+                    break;
+                case "Pencil" :
+                    System.out.println("펜");
+                    break;
+                case "Circle" :
+                    System.out.println("원");
+                    break;
+                case "Rectangle" :
+                    System.out.println("사각형");
+                    break;
             }
-            // TODO : mTools 이벤트처리하기
         }
     }
 
