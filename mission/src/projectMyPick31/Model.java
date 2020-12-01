2c7826e361570e2fd3226a437738afe0e271d224
bibi6666667 <bibi6666667@kakao.com>
2020-12-01 17:42:21 +0900

    [개인프로젝트] Model - season 플레이버 txt파일 완성 및 불러오기 성공
    - season플레이버 txt파일 완성
    -txt파일로 콘솔에 불러오기 성공
    - 과한 인스턴스변수 삭제
    - txt파일 내용을 담아올 자료구조로 ArrayList 공부 및 셋팅

--- mission/src/projectMyPick31/Model.java
+++ mission/src/projectMyPick31/Model.java
@@ -1,11 +1,13 @@
 package projectMyPick31;
 
 import java.io.*;
+import java.util.ArrayList;
 
 
 // Model :  데이터를 다루는 객체 ) txt파일I/O, 카테고리 데이터 필터링 일부를 담당할 것이다.
 public class Model {
-    File signature = new File("C:\\Users\\rnala\\OneDrive\\바탕 화면\\bibi\\Codesquad-Cocoa-JAVA\\mission\\src\\projectMyPick31\\signature.txt");
+    File signatures = new File("C:\\Users\\rnala\\OneDrive\\바탕 화면\\bibi\\Codesquad-Cocoa-JAVA\\mission\\src\\projectMyPick31\\signature.txt");
+    File seasons = new File("C:\\Users\\rnala\\OneDrive\\바탕 화면\\bibi\\Codesquad-Cocoa-JAVA\\mission\\src\\projectMyPick31\\season.txt");
 
     String[] allBases = {"바닐라", "초콜릿", "치즈", "민트", "녹차",
             "요거트", "견과류", "커피", "카라멜", "우유",
@@ -15,23 +17,27 @@ public class Model {
             "초콜릿", "초콜릿 칩", "크런치 초콜릿", "초코볼",
             "과자", "케이크", "기타"};
     String[] allSyrups = {"초콜릿", "카라멜", "과일", "기타"};
-    
-    int allBaseLength = allBases.length;
-    int allToppingLength = allToppings.length;
-    int allSyrupLength = allSyrups.length;
 
-    // Signature와 Seaosn을 담을 자료구조가 필요 - 배열 외에..
-    String[] allSignatures;
-    String[] allSeasons;
+    ArrayList<String> allSignatures = new ArrayList<>(20);
+    ArrayList<String> allSeasons = new ArrayList<>(200);
+    ArrayList<String> allSignaturesNSeasons = new ArrayList<>(220);
 
+    ArrayList<String> baseFilteredFlavor = new ArrayList<>();
+    ArrayList<String> toppingFilteredFlavor = new ArrayList<>();
+    ArrayList<String> syrupFilteredFlavor = new ArrayList<>();
+
+    void makeAllSignaturesNSeasons(){
+        allSignaturesNSeasons.addAll(allSignatures);
+        allSignaturesNSeasons.addAll(allSeasons);
+    }
 
     void readSignatures(){
         try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(signature));
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(signatures));
             while(true) {
                 String line = bufferedReader.readLine();
                 if (line == null) break;
-                System.out.println(line);
+                allSignatures.add(line);
             }
             bufferedReader.close();
         } catch (IOException e) {
@@ -40,7 +46,29 @@ public class Model {
     }
 
     void readSeasons(){
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(seasons));
+            while(true) {
+                String line = bufferedReader.readLine();
+                if (line == null) break;
+                allSeasons.add(line);
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    void filterBase(String baseInput){
+        System.out.println(baseInput);
+    }
+
+    void filterTopping(String toppingInput){
+        System.out.println(toppingInput);
+    }
+
+    void filterSyrup(String syrupInput){
+        System.out.println(syrupInput);
     }
 
 }
