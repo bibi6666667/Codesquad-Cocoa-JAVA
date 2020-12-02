645852e55fc02594067eb55f4fa218ee6faae7c7
bibi6666667 <bibi6666667@kakao.com>
2020-12-02 18:44:42 +0900

    [개인프로젝트] Controller - 중복제거
    여러 베이스 선택했을 때 중복 제거를 위한 로직 구현!
    https://lnsideout.tistory.com/entry/JAVA-%EC%9E%90%EB%B0%94-%EB%B0%B0%EC%97%B4-ArrayList-%EC%A4%91%EB%B3%B5%EC%A0%9C%EA%B1%B0-%EB%AA%A8%EB%93%A0%EB%B0%A9%EB%B2%95 를 참고했습니다. 감사합니다.
    매우 유용한 로직인 듯 하니 활용법을 기억해둬야겠다.

--- mission/src/projectMyPick31/Controller.java
+++ mission/src/projectMyPick31/Controller.java
@@ -13,7 +13,7 @@ public class Controller {
         controller.ready();
     }
 
-    void ready(){
+    void ready() {
         model.readSignatures();
         model.readSeasons();
         model.makeAllSignaturesNSeasons();
@@ -29,15 +29,15 @@ public class Controller {
         home();
     }
 
-    void home(){
+    void home() {
         view.start();
         int input = view.home();
         switch (input) {
-            case 1 :
+            case 1:
                 int viewInput = view.printWhat();
                 printWhat(viewInput);
                 break;
-            case 2 :
+            case 2:
                 String baseInput = view.chooseBase();
                 String toppingInput = view.chooseTopping();
                 String syrupInput = view.chooseSyrup();
@@ -49,10 +49,10 @@ public class Controller {
                 view.returnHome();
                 home();
                 break;
-            case 3 :
+            case 3:
                 view.bye();
                 break;
-            default :
+            default:
                 view.inputAgain();
                 home();
                 break;
@@ -61,27 +61,27 @@ public class Controller {
 
     void printWhat(int input) {
         switch (input) {
-            case 1 :
+            case 1:
                 view.printSignatures();
                 view.returnHome();
                 home();
                 break;
-            case 2 :
+            case 2:
                 view.printSeasons();
                 view.returnHome();
                 home();
                 break;
-            default :
+            default:
                 view.inputAgain();
                 view.printWhat();
                 break;
         }
     }
 
-    String[] transferBaseInput(String baseInputNum){
+    String[] transferBaseInput(String baseInputNum) {
         String[] baseInputNumArr = baseInputNum.split(",");
         String[] baseInputStrArr = new String[3]; // 3개까지 선택가능
-        for(int i = 0; i < baseInputNumArr.length; i++){ // 숫자배열의 0번째부터.
+        for (int i = 0; i < baseInputNumArr.length; i++) { // 숫자배열의 0번째부터.
             int toTransfer = Integer.parseInt(baseInputNumArr[i]); // 0, 1, 2..번쨰 (Str->Int)
             String transfered = model.bases[toTransfer - 1]; //
             baseInputStrArr[i] = transfered;
@@ -89,34 +89,45 @@ public class Controller {
         return baseInputStrArr;
     }
 
-    ArrayList<String> filterBase(String[] transferredBaseInput){// 최소 1개, 최대 3개 선택 가능
+    ArrayList<String> filterBase(String[] transferredBaseInput) {// 최소 1개, 최대 3개 선택 가능
         int baseInputArrLength = transferredBaseInput.length;
         ArrayList<String> allSignaturesNSeasons = model.allSignaturesNSeasons;
         int allSignaturesNSeasonsSize = allSignaturesNSeasons.size();
-        ArrayList<String> filteredBaseResult = new ArrayList<>(); // 필터링 결과값 : 플레이버명
-        for(int i = 0; i < baseInputArrLength; i++) { // for1. 선택 베이스목록 각각에 대해..
+        ArrayList<String> filteredBases = new ArrayList<>(); // 중간 결과값
+        int filteredBasesSize = filteredBases.size();
+        ArrayList<String> filteredResult = new ArrayList<>(); // 중복제거한 최종 결과값
+        for (int i = 0; i < baseInputArrLength; i++) { // for1. 선택 베이스목록 각각에 대해..
             String aBaseInput = transferredBaseInput[i];
             if (aBaseInput == null) break;
             System.out.println(aBaseInput + "를/을 찾습니다.");
             for (int j = 0; j < allSignaturesNSeasonsSize; j++) { // for2. 시그니처/시즌에서 각 베이스와 일치하는 플레이버 찾기
                 String aSignatureOrSeason = allSignaturesNSeasons.get(j);
                 String comparedBase = aSignatureOrSeason.split("/")[3]; // 베이스는 인덱스3
-                if (comparedBase.contains(aBaseInput)){
+                if (comparedBase.contains(aBaseInput)) {
                     String result = aSignatureOrSeason.split("/")[1]; // 플레이버명은 인덱스1
-                    filteredBaseResult.add(result);
+                    filteredBases.add(result);
+                    // System.out.println(result);
                 }
             }
         }
-        return filteredBaseResult;
+        // 중복제거
+        for (int k = 0; k < filteredBases.size(); k++) {
+            String filteredBase = filteredBases.get(k);
+            if (!filteredResult.contains(filteredBase)) {
+                filteredResult.add(filteredBase);
+            }
+        }
+        return filteredResult;
     }
 
-    void filterTopping(String toppingInput){ // 최대 2개
+    void filterTopping(String toppingInput) { // 최대 2개
     }
 
-    void filterSyrup(String syrupInput){ // 최대 1개
+    void filterSyrup(String syrupInput) { // 최대 1개
     }
 
-    void filterResult(ArrayList<String> baseResult){ // filterBase, filterTopping, filterSyrup의 교집합 보여주기
+    void filterResult(ArrayList<String> baseResult) { // filterBase, filterTopping, filterSyrup의 교집합 보여주기
+        //// 기능구현 끝나면 아래 출력부분은 View로 옮기기
         int baseResultSize = baseResult.size();
         System.out.println("총 " + baseResultSize + "개의 플레이버가 검색되었습니다.");
         for (int i = 0; i < baseResultSize; i++) {
