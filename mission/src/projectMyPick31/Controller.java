be5dc357a1636e9741ce8a102feed21b3ce5b72f
bibi6666667 <bibi6666667@kakao.com>
2020-12-03 15:49:34 +0900

    [개인프로젝트] Controller - 토핑, 시럽 필터링 로직 및 결과 로직 구현(ing)
    - 잘 동작하는 베이스 필터링 로직을 토핑, 시럽에도 적용.
    - 결과값을 조회하는 로직 만드는 중. 교집합 조회&중복제거 필요.

--- mission/src/projectMyPick31/Controller.java
+++ mission/src/projectMyPick31/Controller.java
@@ -41,11 +41,13 @@ public class Controller {
                 String baseInput = view.chooseBase();
                 String toppingInput = view.chooseTopping();
                 String syrupInput = view.chooseSyrup();
-                ArrayList<String> transferredBaseInput = manageBaseInput(baseInput);
-                ArrayList<String> filteredBaseResult = filterBase(transferredBaseInput);
-                filterTopping(toppingInput);
-                filterSyrup(syrupInput);
-                filterResult(filteredBaseResult);
+                ArrayList<String> managedBaseInput = manageBaseInput(baseInput);
+                ArrayList<String> filteredBaseResult = filterBase(managedBaseInput);
+                ArrayList<String> managedToppingInput = manageToppingInput(toppingInput);
+                ArrayList<String> filteredToppingResult = filterTopping(managedToppingInput);
+                ArrayList<String> managedSyrupInput = manageSyrupInput(syrupInput);
+                ArrayList<String> filteredSyrupResult = filterSyrup(managedSyrupInput);
+                filterResult(filteredBaseResult, filteredToppingResult ,filteredSyrupResult);
                 view.returnHome();
                 home();
                 break;
@@ -80,8 +82,11 @@ public class Controller {
 
     ArrayList<String> manageBaseInput(String baseInputNum) {
         String[] baseInputNumArr = baseInputNum.split(",");
-        String[] baseInputStrArr = new String[3]; // 3개까지 선택가능
         ArrayList<String> managedBaseInput = new ArrayList<>(); // 최종결과값(선택값+선택하위값)
+        if (baseInputNumArr[0].equals("")) { // 입력 없으면 값 없이 반환
+            System.out.println("베이스 입력 없음");
+            return managedBaseInput;
+        }
         for (int i = 0; i < baseInputNumArr.length; i++) { // 숫자배열의 0번째부터.
             int toTransfer = Integer.parseInt(baseInputNumArr[i]); // 0, 1, 2..번쨰 (Str->Int)
             String transfered = model.bases[toTransfer - 1]; //
@@ -95,24 +100,20 @@ public class Controller {
                 managedBaseInput.add(base2Arr[j]);
             }
         }
-        // TODO : baseInputStrArr의 각 항목 하위카테고리 찾아오
-        //print
-        for(String baseResult : managedBaseInput) {
-            System.out.println(baseResult);
-        }
         return managedBaseInput;
     }
 
-    ArrayList<String> filterBase(ArrayList<String> transferredBaseInput) {// 최소 1개, 최대 3개 선택 가능
-        int baseInputSize = transferredBaseInput.size();
-        ArrayList<String> allSignaturesNSeasons = model.allSignaturesNSeasons;
-        int allSignaturesNSeasonsSize = allSignaturesNSeasons.size();
+    ArrayList<String> filterBase(ArrayList<String> managedBaseInput) {// 최대 3개 선택 가능
+        int baseInputSize = managedBaseInput.size();
         ArrayList<String> filteredBases = new ArrayList<>(); // 중간 결과값
         ArrayList<String> filteredResult = new ArrayList<>(); // 중복제거한 최종 결과값
+        if (baseInputSize == 0) {
+            return filteredResult;
+        }
+        ArrayList<String> allSignaturesNSeasons = model.allSignaturesNSeasons;
+        int allSignaturesNSeasonsSize = allSignaturesNSeasons.size();
         for (int i = 0; i < baseInputSize; i++) { // for1. 선택 베이스목록 각각에 대해..
-            String aBaseInput = transferredBaseInput.get(i);
-            // if (aBaseInput == null) break;
-            // System.out.println(aBaseInput + "를/을 찾습니다.");
+            String aBaseInput = managedBaseInput.get(i);
             for (int j = 0; j < allSignaturesNSeasonsSize; j++) { // for2. 시그니처/시즌에서 각 베이스와 일치하는 플레이버 찾기
                 String aSignatureOrSeason = allSignaturesNSeasons.get(j);
                 String comparedBase = aSignatureOrSeason.split("/")[3]; // 베이스는 인덱스3
@@ -123,8 +124,7 @@ public class Controller {
                 }
             }
         }
-        // 중복제거
-        for (int k = 0; k < filteredBases.size(); k++) {
+        for (int k = 0; k < filteredBases.size(); k++) { // 중복제거
             String filteredBase = filteredBases.get(k);
             if (!filteredResult.contains(filteredBase)) {
                 filteredResult.add(filteredBase);
@@ -133,22 +133,149 @@ public class Controller {
         return filteredResult;
     }
 
-    void filterTopping(String toppingInput) { // 최대 2개
-    }
-
-    void filterSyrup(String syrupInput) { // 최대 1개
-    }
-
-    void filterResult(ArrayList<String> baseResult) { // filterBase, filterTopping, filterSyrup의 교집합 보여주기
-        //// 기능구현 끝나면 아래 출력부분은 View로 옮기기
-        int baseResultSize = baseResult.size();
-        System.out.println("총 " + baseResultSize + "개의 플레이버가 검색되었습니다.");
-        for (int i = 0; i < baseResultSize; i++) {
-            System.out.print(baseResult.get(i));
-            if (i == baseResultSize - 1) continue;
-            System.out.print(",");
-            if ((i + 1) % 5 == 0) System.out.println(""); // 5개씩 한 줄로 출력
+    ArrayList<String> manageToppingInput(String toppingInputNum){
+        String[] toppingInputNumArr = toppingInputNum.split(",");
+        ArrayList<String> managedToppingInput = new ArrayList<>(); // 최종 결과값(선택+하위값)
+        if (toppingInputNumArr[0].equals("")) { // 입력 없으면 값 없이 반환
+            System.out.println("토핑 입력 없음");
+            return managedToppingInput;
         }
+        for (int i = 0; i < toppingInputNumArr.length; i++) { // 숫자배열의 0번째부터.
+            int toTransfer = Integer.parseInt(toppingInputNumArr[i]); // 0, 1, 2..번쨰 (Str->Int)
+            String transfered = model.toppings[toTransfer - 1]; //
+            managedToppingInput.add(transfered);
+            // 하위카테고리
+            String topping2Str = model.allToppings.get(transfered); // 선택한토핑 1개가 키인 값(하위카테고리)
+            if (topping2Str == "") continue; // 하위카테고리 없으면 스킵
+            System.out.println(transfered + "의 하위카테고리는 " + topping2Str);
+            String[] topping2Arr = topping2Str.split(",");
+            for(int j = 0; j < topping2Arr.length; j++) {
+                managedToppingInput.add(topping2Arr[j]);
+            }
+        }
+        return managedToppingInput;
+    }
+
+    ArrayList<String> filterTopping(ArrayList<String> managedToppingInput) { // 최대 2개
+        int toppingInputSize = managedToppingInput.size();
+        ArrayList<String> filteredToppings = new ArrayList<>(); // 중간 결과값
+        ArrayList<String> filteredResult = new ArrayList<>(); // 중복제거한 최종 결과값
+        if (toppingInputSize == 0) {
+            return filteredResult;
+        }
+        ArrayList<String> allSignaturesNSeasons = model.allSignaturesNSeasons;
+        int allSignaturesNSeasonsSize = allSignaturesNSeasons.size();
+        for (int i = 0; i < toppingInputSize; i++) { // for1. 선택 토핑목록 각각에 대해..
+            String aToppingInput = managedToppingInput.get(i);
+            // if (aToppingInput == null) break;
+            for (int j = 0; j < allSignaturesNSeasonsSize; j++) { // for2. 시그니처/시즌에서 각 토핑과 일치하는 플레이버 찾기
+                String aSignatureOrSeason = allSignaturesNSeasons.get(j);
+                String comparedTopping = aSignatureOrSeason.split("/")[4]; // 토핑은 인덱스4
+                if (comparedTopping.contains(aToppingInput)) {
+                    String result = aSignatureOrSeason.split("/")[1]; // 플레이버명은 인덱스1
+                    filteredToppings.add(result);
+                    // System.out.println(result);
+                }
+            }
+        }
+        for (int k = 0; k < filteredToppings.size(); k++) { // 중복제거
+            String filteredTopping = filteredToppings.get(k);
+            if (!filteredResult.contains(filteredTopping)) {
+                filteredResult.add(filteredTopping);
+            }
+        }
+        return filteredResult;
+    }
+
+    ArrayList<String> manageSyrupInput(String syrupInputNum){
+        String[] syrupInputNumArr = syrupInputNum.split(",");
+        ArrayList<String> managedSyrupInput = new ArrayList<>(); // 최종 결과값(선택+하위값)
+        if (syrupInputNumArr[0].equals("")) { // 입력 없으면 값 없이 반환
+            System.out.println("시럽 입력 없음");
+            return managedSyrupInput;
+        }
+        for (int i = 0; i < syrupInputNumArr.length; i++) { // 숫자배열의 0번째부터.
+            int toTransfer = Integer.parseInt(syrupInputNumArr[i]); // 0, 1, 2..번쨰 (Str->Int)
+            String transfered = model.syrups[toTransfer - 1]; //
+            managedSyrupInput.add(transfered);
+            // 하위카테고리
+            String syrup2Str = model.allSyrups.get(transfered); // 선택한토핑 1개가 키인 값(하위카테고리)
+            if (syrup2Str == "") continue; // 하위카테고리 없으면 스킵
+            System.out.println(transfered + "의 하위카테고리는 " + syrup2Str);
+            String[] syrup2Arr = syrup2Str.split(",");
+            for(int j = 0; j < syrup2Arr.length; j++) {
+                managedSyrupInput.add(syrup2Arr[j]);
+            }
+        }
+        return managedSyrupInput;
+    }
+
+    ArrayList<String> filterSyrup(ArrayList<String> managedSyrupInput) { // 최대 1개
+        int syrupInputSize = managedSyrupInput.size();
+        ArrayList<String> filteredSyrups = new ArrayList<>(); // 중간 결과값
+        ArrayList<String> filteredResult = new ArrayList<>(); // 중복제거한 최종 결과값
+        if (syrupInputSize == 0) {
+            return filteredResult;
+        }
+        ArrayList<String> allSignaturesNSeasons = model.allSignaturesNSeasons;
+        int allSignaturesNSeasonsSize = allSignaturesNSeasons.size();
+        for (int i = 0; i < syrupInputSize; i++) { // for1. 선택 시럽목록 각각에 대해..
+            String aSyrupInput = managedSyrupInput.get(i);
+            // if (aSyrupInput == null) break;
+            for (int j = 0; j < allSignaturesNSeasonsSize; j++) { // for2. 시그니처/시즌에서 각 시럽과 일치하는 플레이버 찾기
+                String aSignatureOrSeason = allSignaturesNSeasons.get(j);
+                String comparedSyrup = aSignatureOrSeason.split("/")[5]; // 시럽은 인덱스5
+                if (comparedSyrup.contains(aSyrupInput)) {
+                    String result = aSignatureOrSeason.split("/")[1]; // 플레이버명은 인덱스1
+                    filteredSyrups.add(result);
+                    // System.out.println(result);
+                }
+            }
+        }
+        for (int k = 0; k < filteredSyrups.size(); k++) { // 중복제거
+            String filteredSyrup = filteredSyrups.get(k);
+            if (!filteredResult.contains(filteredSyrup)) {
+                filteredResult.add(filteredSyrup);
+            }
+        }
+        return filteredResult;
+    }
+
+    void filterResult(ArrayList<String> baseResult, ArrayList<String> toppingResult, ArrayList<String> syrupResult) { // filterBase, filterTopping, filterSyrup의 교집합 보여주기
+        ArrayList<String> filteredBaseToppingSyrup = new ArrayList<>();
+        System.out.println("베이스필터링결과 " + baseResult.size());
+        System.out.println("토핑필터링결과 " + toppingResult.size());
+        System.out.println("시럽필터링결과 " + syrupResult.size());
+        filteredBaseToppingSyrup.addAll(baseResult);
+        filteredBaseToppingSyrup.addAll(toppingResult);
+        filteredBaseToppingSyrup.addAll(syrupResult);
+        System.out.println("filteredBaseToppingSyrup 갯수 " + filteredBaseToppingSyrup.size());
+        ArrayList<String> matchedAll = new ArrayList<>(); // 베이스,토핑,시럽 교집합인 결과값
+        ArrayList<String> finalResult = new ArrayList<>(); // 중복제거된 최종 결과값
+        for (int i = 0; i < filteredBaseToppingSyrup.size(); i++) { // 교집합 추출
+            String isMatched = filteredBaseToppingSyrup.get(i);
+            if (baseResult.contains(isMatched) && toppingResult.contains(isMatched) && syrupResult.contains(isMatched)) { // 베이스 토핑 시럽 결과값에 모두 존재하면
+                matchedAll.add(isMatched);
+            }
+        }
+        System.out.println("matchedAll 갯수 " + matchedAll.size());
+        for (int i = 0; i < matchedAll.size(); i++) {
+            String isDuplicated = matchedAll.get(i);
+            if (! finalResult.contains(isDuplicated)) {
+                finalResult.add(isDuplicated);
+            }
+        }
+        System.out.println("finalResult 갯수 " + finalResult.size());
+        //// 최종출력 ) 출력부분은 View로 옮기기
+        int finalResultSize = finalResult.size();
+        System.out.println("총 " + finalResultSize + "개의 플레이버가 검색되었습니다.");
+        for (int i = 0; i < finalResultSize; i++) {
+            System.out.print(finalResult.get(i));
+            if (i == finalResultSize - 1) continue; // 맨 끝에는 쉼표 생략
+            System.out.print(",");
+            if ((i + 1) % 5 == 0) System.out.println(""); // 5항목 출력 후 개행
+        }
+
         System.out.println("\n검색이 완료되었습니다.");
     }
 }
